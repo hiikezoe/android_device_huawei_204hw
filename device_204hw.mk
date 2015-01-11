11809f338f8340dd198a87fbb54bad2a6f7d0c2f
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2015-01-12 07:51:08 +0900

    Move init.huawei.rc to rootdir/.

--- device_204hw.mk
+++ device_204hw.mk
@@ -11,7 +11,7 @@ LOCAL_PATH := device/huawei/204hw
 
 PRODUCT_COPY_FILES += \
   $(LOCAL_PATH)/fstab.huawei:root/fstab.huawei \
-  $(LOCAL_PATH)/init.huawei.rc:root/init.huawei.rc
+  $(LOCAL_PATH)/rootdir/init.huawei.rc:root/init.huawei.rc
 
 $(call inherit-product, build/target/product/full.mk)
 
