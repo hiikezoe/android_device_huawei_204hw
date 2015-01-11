09d59f398e30df5d8d7ee40cfb41d6b1b5ea4837
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2015-01-12 07:50:09 +0900

    Build hardware libraries.

--- device_204hw.mk
+++ device_204hw.mk
@@ -15,6 +15,13 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, build/target/product/full.mk)
 
+PRODUCT_PACKAGES += \
+  libgenlock \
+  copybit.msm7x27a \
+  gralloc.msm7x27a \
+  hwcomposer.msm7x27a \
+  libtilerenderer
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_204hw
 PRODUCT_DEVICE := 204hw
