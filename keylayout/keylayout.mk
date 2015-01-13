05f66c8408d62c6f592786487661a60f0bd02baa
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2015-01-13 21:47:46 +0900

    Add y300j1_keypad.kl. Now '入/切' key works as HOME key.

--- keylayout/keylayout.mk
+++ keylayout/keylayout.mk
@@ -3,4 +3,5 @@ LOCAL_PATH := device/huawei/204hw
 PRODUCT_COPY_FILES += \
   $(LOCAL_PATH)/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
   $(LOCAL_PATH)/keylayout/7x27a_kp.kl:system/usr/keylayout/7x27a_kp.kl \
+  $(LOCAL_PATH)/keylayout/y300j1_keypad.kl:system/usr/keylayout/y300j1_keypad.kl \
   $(LOCAL_PATH)/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
