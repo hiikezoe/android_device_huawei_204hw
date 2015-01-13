e774a5c6e96fc5d7eb99a8c92f98290561390fdd
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2015-01-13 21:47:06 +0900

    Remove unnecessary keymap and keylayout.

--- keychars/keychars.mk
+++ keychars/keychars.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := device/huawei/204hw
 
 PRODUCT_COPY_FILES += \
   $(LOCAL_PATH)/keychars/7x27a_kp.kcm:system/usr/keychars/7x27a_kp.kcm \
-  $(LOCAL_PATH)/keychars/Generic.kcm:system/usr/keychars/Generic.kcm \
   $(LOCAL_PATH)/keychars/Virtual.kcm:system/usr/keychars/Virtual.kcm \
   $(LOCAL_PATH)/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
   $(LOCAL_PATH)/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm
--- keylayout/keylayout.mk
+++ keylayout/keylayout.mk
@@ -3,16 +3,4 @@ LOCAL_PATH := device/huawei/204hw
 PRODUCT_COPY_FILES += \
   $(LOCAL_PATH)/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
   $(LOCAL_PATH)/keylayout/7x27a_kp.kl:system/usr/keylayout/7x27a_kp.kl \
-  $(LOCAL_PATH)/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-  $(LOCAL_PATH)/keylayout/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_028e.kl \
-  $(LOCAL_PATH)/keylayout/Vendor_046d_Product_c216.kl:system/usr/keylayout/Vendor_046d_Product_c216.kl \
-  $(LOCAL_PATH)/keylayout/Vendor_046d_Product_c294.kl:system/usr/keylayout/Vendor_046d_Product_c294.kl \
-  $(LOCAL_PATH)/keylayout/Vendor_046d_Product_c299.kl:system/usr/keylayout/Vendor_046d_Product_c299.kl \
-  $(LOCAL_PATH)/keylayout/Vendor_046d_Product_c532.kl:system/usr/keylayout/Vendor_046d_Product_c532.kl \
-  $(LOCAL_PATH)/keylayout/Vendor_054c_Product_0268.kl:system/usr/keylayout/Vendor_054c_Product_0268.kl \
-  $(LOCAL_PATH)/keylayout/Vendor_05ac_Product_0239.kl:system/usr/keylayout/Vendor_05ac_Product_0239.kl \
-  $(LOCAL_PATH)/keylayout/Vendor_22b8_Product_093d.kl:system/usr/keylayout/Vendor_22b8_Product_093d.kl \
-  $(LOCAL_PATH)/keylayout/atmel_mxt_ts.kl:system/usr/keylayout/atmel_mxt_ts.kl \
-  $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
-  $(LOCAL_PATH)/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-  $(LOCAL_PATH)/keylayout/surf_keypad.kl:system/usr/keylayout/surf_keypad.kl
+  $(LOCAL_PATH)/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
