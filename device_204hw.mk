f607690bbfe9ef03fd61b44ac23dc168dfdb05dc
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2015-01-21 20:12:51 +0900

    Include phone-hdpi-512-dalvik-heap.mk to set dalvik heap size.

--- device_204hw.mk
+++ device_204hw.mk
@@ -45,3 +45,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_204hw
 PRODUCT_DEVICE := 204hw
 
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
